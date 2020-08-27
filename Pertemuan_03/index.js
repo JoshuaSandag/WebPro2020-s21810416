a869720fac98002f537543723f8ecab55c623f43
JoshuaSandag <s21810416@student.unklab.ac.id>
2020-08-27 15:54:35 +0700

    second commit

--- Pertemuan_03/index.js
+++ Pertemuan_03/index.js
@@ -1,2 +1,30 @@
-console.log ("halo semua")
-console.log ("Pertemuan ke 3 WebPro menyisipkan JS kedalam HTML ")
+/* halo semua joshua disini, 
+kali ini kita akan belajar tentang dasar" pemograman javascript mulai dari
+-menyisipkan JS kedalam HTML 
+-belajar tentang variabel serta tipe data
+-array
+-operator
+cekidot !!!
+*/
+
+const firstName = "joshua"; //deklarasi variabel dengan tipe data string harus menggunakan ("") 
+const lastName = "sandag";//tipe data string
+let age = 20 ; //tipe data number 
+let status = "single"; //tipe data string
+
+console.log(firstName); //output =joshua
+console.log(lastName); //output =sandag
+console.log(age); //output =20
+console.log(status); //output =single
+
+const info = " nama saya adalah " + firstName + lastName + " umur saya " + age + " tahun, status " + status ; //array dapat menampung banyak tipe data   
+console.log(info);
+
+
+
+
+
+
+
+
+ 
