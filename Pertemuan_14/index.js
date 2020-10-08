852e4088a8b269c7f7248544ff24e180e85b6fcd
JoshuaSandag <s21810416@student.unklab.ac.id>
2020-10-08 14:00:02 +0700

    first commit

---
+++ Pertemuan_14/index.js
@@ -0,0 +1,18 @@
+//Destructuring
+// memecah item pada Array
+// memecah properti pada objek
+
+//1. Array
+//a. Menggunakan Destructuring
+// const colors =["merah", "kuning", "biru", "hijau"];
+// let[satu, dua, tiga, empat] = "warna";
+
+//2. Objek
+//a. Menggunakan Destructuring
+// const user = {
+//     nama : "joshua",
+//     kelamin : "laki-laki",
+//     umur : 20
+// };
+// let {nama, kelamin, umur} = user;
+
